497db38cb6ed075f50cb921e7792e7dc805e522d
OlenkaMelnikova <AsuramaruDemon@yandex.ru>
2016-10-30 23:58:48 +0300

    Add files via upload

---
+++ one-dimensional array.cpp
@@ -0,0 +1,32 @@
+// one-dimensional array.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <stdlib.h>
+void main()
+{
+	double *pMas;
+	int n;
+	printf_s("n=");
+	scanf_s("%d", &n);
+	pMas = (double *)malloc(n * sizeof(double));
+	for (int i = 0; i<n; i++)
+	{
+		pMas[i] = rand() % 10;
+		printf("%f ", pMas[i]);
+	}
+	for (int i = 0; i<n / 2; i++)
+	{
+		double buf = pMas[i] * 2;
+		pMas[i] = pMas[n-1-i] * 3;
+		pMas[n-1-i] = buf;
+	}
+	printf("\n");
+	for (int i = 0; i<n; i++)
+		printf("%f ", pMas[i]);
+	free(pMas);
+	system("pause");
+}
+
